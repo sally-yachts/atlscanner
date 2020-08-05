24599dc2edf5415efe60979142d355257e148020
sally-yachts <69201647+sally-yachts@users.noreply.github.com>
2020-08-05 12:02:02 -0400

    Update upload-call.sh

--- configs/trunk-recorder/upload-call.sh
+++ configs/trunk-recorder/upload-call.sh
@@ -2,7 +2,10 @@
 
 basename="${2%.*}"
 
-# Uploads the unedited recording to test rdio-player instance on same machine
+# Uploads the unedited recording to test rdio-player instance on same machine.
+# This allows for quick before/after normalization testing in case there is any suspicion that normalize-audio
+# is causing issues
+
 curl http://127.0.0.1:3000/api/trunk-recorder-call-upload \
      -F "key=secret-key-goes-here" \
      -F "audio=@${basename}.wav;type=audio/wav" \
@@ -11,6 +14,9 @@ curl http://127.0.0.1:3000/api/trunk-recorder-call-upload \
 
 
 # normalizes the audio file
+# If you upload to Broadcastify or OpenMHZ calls are normalized and converted automatically
+# by trunk-recorder and/or the scripts provided by the service, but not if you host everything locally. 
+
 normalize-audio -a -14dBFS "${2}"
 
 # Uploads the normalized file to production rdio-player instance
